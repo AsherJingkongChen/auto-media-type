ef0de21a1966feb5d68503ce28ded3bb9082cc23
AsherJingkongChen <37398747+AsherJingkongChen@users.noreply.github.com>
2024-01-23 20:47:15 +0800

    (doc), (enhance): Add media type doc creator (from template file)

---
+++ docs/media-types/image/bmp.md
@@ -0,0 +1,16 @@
+# [⎗](../README.md) `/`
+
+## File Extensions
+
+``
+
+## Magic Numbers
+
+|      | 0   | 1   | 2   | 3   |
+| ---- | --- | --- | --- | --- |
+| Byte | 0x  | 0x  | 0x  | 0x  |
+| Char |     |     |     |     |
+
+## References
+
+- []()
---
+++ scripts/create-media-type-docs.js
@@ -0,0 +1,24 @@
+#! /usr/bin/env node
+
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+import { cp, mkdir } from 'fs/promises';
+
+const { argv } = process;
+if (argv.length < 3) {
+  throw new Error(
+    'Missing required arguments: "Media types". ' +
+      `Usage: ${argv[0]} ${argv[1]} <Media-types...>`,
+  );
+}
+
+const mediaTypes = argv.slice(2);
+Promise.all(mediaTypes.map(createMediaTypeDoc));
+
+async function createMediaTypeDoc(mediaType) {
+  const docsDir = join(dirname(fileURLToPath(import.meta.url)), '../docs');
+  const docFilePath = join(docsDir, `media-types/${mediaType}.md`);
+  const templateFilePath = join(docsDir, 'templates/media-type.md');
+
+  await cp(templateFilePath, docFilePath, { recursive: true });
+}
