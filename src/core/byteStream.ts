c3571be2dd29860ebd4405cf167be427e0227f72
AsherJingkongChen <37398747+AsherJingkongChen@users.noreply.github.com>
2024-02-14 11:35:49 +0800

    (adjust): Comments

--- src/core/byteStream.ts
+++ src/core/byteStream.ts
@@ -1,9 +1,9 @@
 /**
  * ## Introduction
- * Read the data chunk from a byte stream.
+ * Read the chunk within the given range from a byte stream.
  *
  * ## Parameters
- * - `byteStream`: `ReadableStream<Uint8Array>`
+ * - `byteStream`: `ReadableStream<ArrayBufferView>`
  *   + A byte stream
  *   + The stream will not be cancelled after the function call
  * - `byteLength`: `number`
@@ -19,14 +19,13 @@
  * ## Note
  * - The consumed data size in byte will be `byteOffset + byteLength`
  * - The remaining data will be left unread,
- *   which means the stream will be left
- *   in a readable state after the function call.
+ *   which means the stream will be readable after the function call.
  * - The function uses a loop to continuously read the chunks from stream,
- *   because `ReadableStreamBYOBReader.prototype.read(view)` does not neccessarily
- *   fill the view with data of multiple chunks. The function will resolve it.
+ *   because `ReadableStreamBYOBReader.prototype.read(view)`
+ *   does not neccessarily fill the view. The function will resolve it.
  */
 export async function readByteStream(
-  byteStream: ReadableStream<Uint8Array>,
+  byteStream: ReadableStream<ArrayBufferView>,
   byteLength: number,
   byteOffset: number = 0,
 ): Promise<Uint8Array> {
